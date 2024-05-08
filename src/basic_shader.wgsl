68cf01a7b754724118b91927fdd025924020bd2f
Matt McKenna <matthew.w.mckenna@spiritaero.com>
2024-05-08 13:40:22 -0500

    Added instancing via button-click.

--- src/basic_shader.wgsl
+++ src/basic_shader.wgsl
@@ -10,11 +10,24 @@ struct VertexOutput {
     @location(0) color: vec4<f32>,
 };
 
+struct InstanceInput {
+    @location(5) x_axis: vec4<f32>,
+    @location(6) y_axis: vec4<f32>,
+    @location(7) z_axis: vec4<f32>,
+    @location(8) w_axis: vec4<f32>,
+};
+
 @vertex
-fn vs_main(model: VertexInput) -> VertexOutput {
+fn vs_main(model: VertexInput, instance: InstanceInput) -> VertexOutput {
+    let model_matrix = mat4x4<f32> (
+        instance.x_axis,
+        instance.y_axis,
+        instance.z_axis,
+        instance.w_axis,
+    );
     var out: VertexOutput;
     out.color = model.color;
-    out.clip_position = vec4<f32>(model.position, 1.0);
+    out.clip_position = model_matrix * vec4<f32>(model.position, 1.0);
     return out;
 }
 
--- src/renderizer.rs
+++ src/renderizer.rs
@@ -1,8 +1,12 @@
 use crate::vertex::{MeshVertex, Vertex, TRIANGLE};
 use eframe::{
-    egui_wgpu::wgpu::util::DeviceExt,
-    egui_wgpu::{self, wgpu},
+    egui_wgpu::{
+        self,
+        wgpu::{self, util::DeviceExt},
+    },
+    wgpu::util::RenderEncoder,
 };
+use rand::prelude::*;
 
 pub struct Renderizer;
 
@@ -18,6 +22,30 @@ impl Renderizer {
             label: Some("Vertex Buffer"),
             contents: bytemuck::cast_slice(TRIANGLE),
             usage: wgpu::BufferUsages::VERTEX,
+        });
+        let instances = vec![
+            Instance {
+                pos: glam::Vec3::new(-0.5, -0.5, 0.0),
+                rot: glam::Quat::IDENTITY,
+            },
+            Instance {
+                pos: glam::Vec3::new(0.5, -0.5, 0.0),
+                rot: glam::Quat::IDENTITY,
+            },
+            Instance {
+                pos: glam::Vec3::new(0.0, 0.5, 0.0),
+                rot: glam::Quat::IDENTITY,
+            },
+            Instance {
+                pos: glam::Vec3::new(0.5, 0.5, 0.0),
+                rot: glam::Quat::IDENTITY,
+            },
+        ];
+        let instance_data: Vec<InstanceRaw> = instances.iter().map(Instance::to_raw).collect();
+        let instance_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Instance Buffer"),
+            contents: bytemuck::cast_slice(&instance_data),
+            usage: wgpu::BufferUsages::VERTEX | wgpu::BufferUsages::COPY_DST,
         });
         //let bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
         //    label: Some("Bind Group Layout"),
@@ -52,7 +80,7 @@ impl Renderizer {
             vertex: wgpu::VertexState {
                 module: &shader,
                 entry_point: "vs_main",
-                buffers: &[MeshVertex::desc()],
+                buffers: &[MeshVertex::desc(), InstanceRaw::desc()],
             },
             fragment: Some(wgpu::FragmentState {
                 module: &shader,
@@ -72,15 +100,32 @@ impl Renderizer {
                 pipeline,
                 vertex_buffer,
                 //bind_group,
+                instances,
+                instance_buffer,
             });
         Self
     }
 
-    fn custom_painting(&mut self, ui: &mut egui::Ui) {
-        let (rect, _res) = ui.allocate_exact_size(egui::Vec2::splat(300.0), egui::Sense::drag());
+    fn custom_painting(&mut self, ui: &mut egui::Ui, add_instance: bool) {
+        let (rect, _res) =
+            ui.allocate_exact_size(egui::Vec2::new(1920.0, 1080.0), egui::Sense::drag());
+        let instance = if add_instance {
+            let mut rng = rand::thread_rng();
+            let x = rng.gen::<f32>() - 0.5;
+            let y = rng.gen::<f32>() - 0.5;
+            //let z = rng.gen::<f32>() - 0.5;
+            Some(Instance {
+                pos: glam::Vec3::new(x, y, 0.0),
+                rot: glam::Quat::IDENTITY,
+            })
+        } else {
+            None
+        };
         ui.painter().add(egui_wgpu::Callback::new_paint_callback(
             rect,
-            RenderizerCallback,
+            RenderizerCallback {
+                new_instance: instance,
+            },
         ));
     }
 }
@@ -88,16 +133,19 @@ impl Renderizer {
 impl eframe::App for Renderizer {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
+            let add_instance = ui.button("Add Instance").clicked();
             egui::ScrollArea::both().auto_shrink(false).show(ui, |ui| {
                 egui::Frame::canvas(ui.style()).show(ui, |ui| {
-                    self.custom_painting(ui);
+                    self.custom_painting(ui, add_instance);
                 });
             });
         });
     }
 }
 
-struct RenderizerCallback;
+struct RenderizerCallback {
+    new_instance: Option<Instance>,
+}
 
 impl egui_wgpu::CallbackTrait for RenderizerCallback {
     fn prepare(
@@ -108,8 +156,22 @@ impl egui_wgpu::CallbackTrait for RenderizerCallback {
         _egui_encoder: &mut wgpu::CommandEncoder,
         resources: &mut egui_wgpu::CallbackResources,
     ) -> Vec<wgpu::CommandBuffer> {
-        let resources: &RenderizerResources = resources.get().unwrap();
-        resources.prepare(device, queue);
+        let resources: &mut RenderizerResources = resources.get_mut().unwrap();
+        match &self.new_instance {
+            Some(i) => {
+                resources.instances.push(i.clone());
+                let instance_data: Vec<InstanceRaw> =
+                    resources.instances.iter().map(Instance::to_raw).collect();
+                resources.instance_buffer =
+                    device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+                        label: Some("Instance Buffer"),
+                        contents: bytemuck::cast_slice(&instance_data),
+                        usage: wgpu::BufferUsages::VERTEX | wgpu::BufferUsages::COPY_DST,
+                    });
+                resources.prepare(device, queue, Some(bytemuck::cast_slice(&[i.to_raw()])));
+            }
+            None => resources.prepare(device, queue, None),
+        }
         Vec::new()
     }
 
@@ -128,17 +190,79 @@ struct RenderizerResources {
     pipeline: wgpu::RenderPipeline,
     //bind_group: wgpu::BindGroup,
     vertex_buffer: wgpu::Buffer,
+    instances: Vec<Instance>,
+    instance_buffer: wgpu::Buffer,
 }
 
 impl RenderizerResources {
-    fn prepare(&self, _device: &wgpu::Device, _queue: &wgpu::Queue) {
-        //
+    fn prepare(&self, _device: &wgpu::Device, queue: &wgpu::Queue, instance: Option<&[u8]>) {
+        //if let Some(i) = instance {
+        //    queue.write_buffer(&self.instance_buffer, 0, i);
+        //}
     }
 
     fn paint<'rp>(&'rp self, render_pass: &mut wgpu::RenderPass<'rp>) {
         render_pass.set_pipeline(&self.pipeline);
         render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
-        //render_pass.set_bind_group(0, &self.bind_group, &[]);
-        render_pass.draw(0..3, 0..1);
+        render_pass.set_vertex_buffer(1, self.instance_buffer.slice(..));
+        render_pass.draw(0..3, 0..self.instances.len() as _)
+    }
+}
+
+#[derive(Clone, Debug)]
+struct Instance {
+    pos: glam::Vec3,
+    rot: glam::Quat,
+}
+
+impl Instance {
+    fn to_raw(&self) -> InstanceRaw {
+        let mat = glam::Mat4::from_rotation_translation(self.rot, self.pos);
+        InstanceRaw {
+            x_axis: mat.x_axis.into(),
+            y_axis: mat.y_axis.into(),
+            z_axis: mat.z_axis.into(),
+            w_axis: mat.w_axis.into(),
+        }
+    }
+}
+
+#[repr(C)]
+#[derive(Copy, Clone, bytemuck::Pod, bytemuck::Zeroable)]
+struct InstanceRaw {
+    x_axis: [f32; 4],
+    y_axis: [f32; 4],
+    z_axis: [f32; 4],
+    w_axis: [f32; 4],
+}
+
+impl InstanceRaw {
+    fn desc() -> wgpu::VertexBufferLayout<'static> {
+        wgpu::VertexBufferLayout {
+            array_stride: std::mem::size_of::<Self>() as wgpu::BufferAddress,
+            step_mode: wgpu::VertexStepMode::Instance,
+            attributes: &[
+                wgpu::VertexAttribute {
+                    offset: 0,
+                    shader_location: 5,
+                    format: wgpu::VertexFormat::Float32x4,
+                },
+                wgpu::VertexAttribute {
+                    offset: std::mem::size_of::<[f32; 4]>() as wgpu::BufferAddress,
+                    shader_location: 6,
+                    format: wgpu::VertexFormat::Float32x4,
+                },
+                wgpu::VertexAttribute {
+                    offset: std::mem::size_of::<[f32; 8]>() as wgpu::BufferAddress,
+                    shader_location: 7,
+                    format: wgpu::VertexFormat::Float32x4,
+                },
+                wgpu::VertexAttribute {
+                    offset: std::mem::size_of::<[f32; 12]>() as wgpu::BufferAddress,
+                    shader_location: 8,
+                    format: wgpu::VertexFormat::Float32x4,
+                },
+            ],
+        }
     }
 }
